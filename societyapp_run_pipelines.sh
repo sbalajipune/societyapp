521dc3c0f3ea8e50b226101c0dad46d7ea6c1152
Balaji Londhe <blondhe1@in.ibm.com>
2019-12-09 13:57:42 +0530

    Creating nexus separatly

--- societyapp_run_pipelines.sh
+++ societyapp_run_pipelines.sh
@@ -1,10 +1,15 @@
 #!/bin/sh
-echo "Creating society app objects and pipelines"
+echo "Running the build and deployment pipelines"
 oc start-build member-app-pipeline
+echo "Wait for 10 mins for this pipeline to finish so that other builds will be successful as nexus would have cached the maven artifacts by then"
 #wait for this pipeline to finish so that other builds will be successful as nexus would have cached the maven artifacts by then
-sleep 60000
+sleep 600
 oc start-build vehicle-app-pipeline
-sleep 6000
+sleep 60
+echo "Wait for 1 min to finish the build"
 oc start-build parking-app-pipeline
-sleep 6000
+echo "Wait for 1 min to finish the build"
+sleep 60
 oc start-build apartment-app-pipeline
+echo "Wait for 1 min to finish the build"
+sleep 60
